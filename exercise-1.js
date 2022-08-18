8b073c8b56c8b26f369165c400ffa85ebdfbb181
Parry93 <parrinelloemanuele@gmail.com>
2022-08-18 23:52:17 +0200

    es11

---
+++ exercise-1.js
@@ -0,0 +1,13 @@
+// const sum = function (a, b) {
+//   return a + b;
+// }
+const sum = (a, b) => 2 + 5;
+
+//  const log = function (value) {
+//   console.log(value);
+// }
+const log = (value) => console.log(value);
+
+
+
+log(sum(2, 5));
