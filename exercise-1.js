354e7330be9a69c65f8c5c310c57add7f3099df1
Parry93 <parrinelloemanuele@gmail.com>
2022-09-14 15:53:42 +0200

    es.41

---
+++ exercise-1.js
@@ -0,0 +1,40 @@
+class BankAccount {
+  #amount = 0;
+
+  constructor(initialAmount) {
+    this.#amount = initialAmount;
+  }
+
+  deposit(amount) {
+    // throw an exception if amount is negative    
+    if (amount < 0) {
+      throw new Error('Amount cannot be negative');
+    }
+
+    this.#amount += amount;
+  }
+
+  withdraw(amount) {
+    // throw an exception if amount is negative or if withdrawal amount is greater than current amount
+    if (amount < 0) {
+      throw new Error("Amount cannot be negative");
+    }
+
+    if (this.#amount < amount) {
+      throw new Error("don't have money money");
+    }
+
+    this.#amount -= amount;
+  }
+  
+
+  view() {
+    console.log(this.#amount);
+  }
+}
+
+const bankAccount = new BankAccount(1000);
+bankAccount.deposit(500);
+bankAccount.deposit(200);
+bankAccount.withdraw(10000); // This operation should not be possible, because you cannot withdraw more than the account balance
+bankAccount.view();
